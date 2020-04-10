6cc88d06f0a825b4ecd548902880fc6873244de5
Juliann Stainbrook <=>
2020-04-10 11:44:33 -0400

    Updates

---
+++ precache-manifest.1946e9e2c5b1270e3fc1fea73e33f430.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "689b18785eae1f3219dae07325a1a13a",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "92037b1b285c8f9d58cd",
+    "url": "/monsters-rolodex/static/css/main.80918338.chunk.css"
+  },
+  {
+    "revision": "926634218f0cd358d7da",
+    "url": "/monsters-rolodex/static/js/2.a6bff23a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.a6bff23a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92037b1b285c8f9d58cd",
+    "url": "/monsters-rolodex/static/js/main.dd0f65fc.chunk.js"
+  },
+  {
+    "revision": "fc1a59712d7f10d18ab0",
+    "url": "/monsters-rolodex/static/js/runtime-main.664c0a0d.js"
+  }
+]);
